6d9ee14bdedb851a87247258c2cd5db667df4d5f
Your Name <drian@ayodyads.com>
2025-07-29 11:06:16 +0700

    chore(config): change to my personal data

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://drianlabs.vercel.app",
+  url: "https://drianlabs.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -23,7 +23,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  projectName: "drianlabs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -73,9 +73,10 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "My Site",
+      style: "dark",
+      title: "Drian Labs",
       logo: {
-        alt: "My Site Logo",
+        alt: "Drian Labs Logo",
         src: "img/logo.svg",
       },
       items: [
@@ -87,8 +88,13 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/driannaird",
           label: "GitHub",
+          position: "right",
+        },
+        {
+          href: "https://www.instagram.com/driannaird",
+          label: "Instagram",
           position: "right",
         },
       ],
@@ -106,19 +112,15 @@ const config: Config = {
           ],
         },
         {
-          title: "Community",
+          title: "Social",
           items: [
             {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              label: "Github",
+              href: "https://github.com/driannaird",
             },
             {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "X",
-              href: "https://x.com/docusaurus",
+              label: "Instagram",
+              href: "https://www.instagram.com/driannaird",
             },
           ],
         },
@@ -129,14 +131,10 @@ const config: Config = {
               label: "Blog",
               to: "/blog",
             },
-            {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
-            },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Drian Labs, Inc.`,
     },
     prism: {
       theme: prismThemes.github,
